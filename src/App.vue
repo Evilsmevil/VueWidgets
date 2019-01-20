d98aace2e1962f55ed11a64b26db112c8fae51b3
Tom Watson <tbwatson@gmail.com>
2019-01-19 21:56:46 -0800

    Added slider

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Slider</h1>
+    <div style="width:25%; display:block; margin-left:auto; margin-right:auto;">
+      <Slider sliderName="Opacity" v-bind:initialValue="sliderInitialValue" v-model="sliderVal" />
+    </div>
+    <h1> {{sliderVal}} </h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Slider from './components/Slider.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Slider,
   },
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+  private sliderInitialValue : number = 13;
+  private sliderVal : number = this.sliderInitialValue;
+
+}
+
 </script>
 
 <style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class = "slidercontainer">
+        <span class = "sliderName"> {{sliderName}} </span>
+        <input v-model="value" v-on:input="$emit('input', $event.target.value)" type="range" min="1" max="100" class="slider">
+        <span class = "valueLabel"> {{value}} </span>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Slider extends Vue {
+
+@Prop() private initialValue! : number;
+@Prop() private sliderName! : string;
+
+private value : number = 0;
+
+private onClick(params: Event) : void {
+      console.log("Hi!" + params.timeStamp);
+  }
+
+  data () {
+      return {
+          value : this.initialValue
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.slidercontainer {
+  width: 100%; /* Width of the outside container */
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 80%;
+  height: 15px;
+  border-radius: 5px;  
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.valueLabel {
+    width:  10%;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%; 
+  background: #4CAF50;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #4CAF50;
+  cursor: pointer;
+}
+
+/* Mouse-over effects */
+.slider:hover {
+  opacity: 1; /* Fully shown on mouse-over */
+}
+</style>
